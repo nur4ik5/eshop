630f0f45d12712f1cd8efc649e0c1397cb171d9b
Nurjigit <sagendikov45@gmail.com>
2022-01-12 17:52:17 +0600

    Добавил шаблон

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+# admin.site.register(Product)
+
+# Кастомизация админки
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ['name', 'category', 'count',]
+	list_filter = ['category']
+	# search_fields = ['name__startswith']
+	search_fields = ['name']
--- products/models.py
+++ products/models.py
@@ -13,18 +13,21 @@ class Product(models.Model):
 		('PHONE', ' Мобильные телефоны'),
 
 	)
-	name = models.CharField(max_length=100)
-	description = models.TextField()
+	name = models.CharField(max_length=100, verbose_name='Название')
+	description = models.TextField(verbose_name='Описание')
 	# для автоматического добавления значения при создании
 	created = models.DateTimeField(auto_now_add=True)
 	# для автоматического изменения значения при обновлении записи
 	updated = models.DateTimeField(auto_now=True)   
 	# для десятичных дробей, пример: 125, 50
-	price = models.DecimalField(max_digits=6, decimal_places=2)   
-
-	count = models.IntegerField()
-	featured_product = models.BooleanField(default=False)
-	category = models.CharField(max_length=5, choices=CATEGORIES, default=PHONE)	
+	price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Цена')   
+	count = models.IntegerField(verbose_name='Количество на складе')
+	featured_product = models.BooleanField(default=False, verbose_name='Продвигать товар')
+	category = models.CharField(max_length=5, choices=CATEGORIES, default=PHONE, verbose_name='Котегория')	
 
 	def __str__(self):
 		return f"{self.name} - {self.category}"
+
+	class Meta:
+		verbose_name='Товар'
+		verbose_name_plural='Товары'
